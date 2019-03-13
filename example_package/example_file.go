9e48fa17daa15de31937994f1db251305b6144d4
dmodi <dmodi@sevone.com>
2019-03-13 14:35:06 +0530

    Added example_package and stringutil

---
+++ example_package/example_file.go
@@ -0,0 +1,5 @@
+package my_example
+
+func Example_sum(a int, b int) int{
+	return a +b
+}
--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,16 @@
+// Package called main is used to create executable binary. Program execution starts in package main by calling its function which also called main.
 package main
 
-import "fmt"
+// one import declaration with two ImportSpecs. Each ImportSpec defines single package import.
+import (
+    "fmt"
+	"GoTutorial/stringutil"  // import path is path relative package’s vendor directory or `go env GOPATH`/src
+	"GoTutorial/example_package"  // Use actual path while importing and use package specified at the start of the file while using var of func
+	// As used my_example.Example_sum
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+
+	fmt.Println(my_example.Example_sum(2, 3))
 }
